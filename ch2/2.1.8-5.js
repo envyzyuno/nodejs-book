fe10c44a1ac861bbbfeb8cde96eda2a54588593e
Ronnie <72782271+envyzyuno@users.noreply.github.com>
2021-04-09 21:32:29 +0900

    2장까지 완료

--- ch2/2.1.8-5.js
+++ ch2/2.1.8-5.js
@@ -1,7 +1,17 @@
+
+
 const promise1 = Promise.resolve('성공1');
 const promise2 = Promise.resolve('성공2');
 (async () => {
+
+  /**
+   * for 와 await 를 함께쓰는것은 
+   * 노드 10 버전 부터 지원하는 ES2018 문법이다.
+   */
   for await (promise of [promise1, promise2]) {
     console.log(promise);
   }
+
 })();
+
+
--- ch2/2.1.8-6.js
+++ ch2/2.1.8-6.js
@@ -1,7 +1,7 @@
 async function findAndSaveUser(Users) {
-  // 생략
+  // 생략 Promise 객체가 응답한다.
 }
-
+/* 비동기처리 방법 1 */
 findAndSaveUser().then(() => { /* 생략 */ });
 // 또는
 async function other() {
