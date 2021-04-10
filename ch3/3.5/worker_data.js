a7a3e8890af0524b4c1e625901eea35cc43d7bdb
Ronnie <72782271+envyzyuno@users.noreply.github.com>
2021-04-10 20:05:20 +0900

    3.5

--- ch3/3.5/worker_data.js
+++ ch3/3.5/worker_data.js
@@ -1,25 +1,44 @@
-const {
-  Worker, isMainThread, parentPort, workerData,
-} = require('worker_threads');
+const { Worker, isMainThread, parentPort, workerData, } = require('worker_threads');
 
 if (isMainThread) { // 부모일 때
   const threads = new Set();
-  threads.add(new Worker(__filename, {
-    workerData: { start: 1 },
-  }));
-  threads.add(new Worker(__filename, {
-    workerData: { start: 2 },
-  }));
+  threads.add(new Worker(__filename, 
+    { workerData: { start: 1 }, } /** workerData 형태로 인수 세팅  */
+    ));
+
+  threads.add(new Worker(__filename, 
+    { workerData: { start: 2 }, } /** workerData 형태로 인수 세팅  */
+    ));
+
+    /** threads set 에 두개의 worker 를 주입  */
+  
   for (let worker of threads) {
+    /** worker message 이벤트 바인드 */
     worker.on('message', message => console.log('from worker', message));
+    /** worker exit 이벤트 바인드 */
     worker.on('exit', () => {
       threads.delete(worker);
       if (threads.size === 0) {
         console.log('job done');
-      }
+      }      
     });
+
   }
 } else { // 워커일 때
   const data = workerData;
-  parentPort.postMessage(data.start + 100);
+  parentPort.postMessage( data.start + 100 );
+  
+  /** 워커가 부모와의 연결을 종료하는 부분이 존재하지 않는다..  
+   *  worker 라는건 
+   *  java Runable 과 같이 한번만 동작하는것인가?
+   *  java Thread while(true) 가 존재 하지 않으면 한번만 동작하고
+   *  종료 되는것인가?
+   * 
+   *  아니다... worker_threads.js 에서 parentPort.close(); 를 지우면
+   *  프로그램이 종료되지 않는다..
+   * 
+   *  worker_threads.js 에서는 워커에서 .on 으로 바인드를 했기 때문에
+   *  워커에서 부모로 close 해야한다.
+   */
+  
 }
--- ch3/3.5/worker_threads.js
+++ ch3/3.5/worker_threads.js
@@ -1,17 +1,52 @@
 const { Worker, isMainThread, parentPort,} = require('worker_threads');
 
+/**
+ * https://melius.tistory.com/58
+ */
 
 if (isMainThread) { // 부모일 때
+  /** 기존에 동작하는 싱글스레드를 메인 스레도 또는 부모 스레드라고 부른다. */
+  /** 현재 물리 js 파일에 대해서 worker 생성 
+   * else { // 워커일때
+   *  아래 부분문 워커쓰레드에서 실행
+   */
   const worker = new Worker(__filename);
+  
+  /** 워커의 message 이벤트 바인드 */
   worker.on('message', message => console.log('from worker', message));
-  worker.on('exit', () => console.log('worker exit'));
+
+  /** 워커의 메세지를 한번만 받고 싶을경우 */
+  //worker.once('message', message => console.log('from worker', message));
+
+  /** 워커의 exit 이벤트 바인드 */
+  worker.on('exit', () => {
+    console.log('worker exit');
+  });
+  
+  
+  /** 워커에 ping 메세지 보냄.  */
   worker.postMessage('ping');
-} else { // 워커일 때
+
+} else { // 워커일때
+  
+  /** 부모 포트  */ 
   parentPort.on('message', (value) => {
     console.log('from parent', value);
-    parentPort.postMessage('pong');
+
+    /** 워커가 부모에게 메세지 pong1 메세지 전송 */
+    parentPort.postMessage('pong1');
+
+    /** 워커가 부모에게 메세지 pong2 메세지 전송 */
+    parentPort.postMessage('pong2');
+
+    /** 
+     * 워커에서 on 을 사용하였기 때문에 워커에서 부모오
+     * 워커가 부모와의 연결을 종료해야 한다.
+     */
     parentPort.close();
   });
+
+
 }
 
 
