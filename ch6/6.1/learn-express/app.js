f0847ff432907aa522f9c6b1643917f3f555cca3
Ronnie <envyzyuno@gmail.com>
2021-04-12 16:10:39 +0900

    p 177

--- ch6/6.1/learn-express/app.js
+++ ch6/6.1/learn-express/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
+app.set('port', process.env.PORT || 3000 );
+
+/** JAVA 의 필터와 같은 기능을 수행한다. */
+app.use( (req, res, next) => {
+  console.log('모든 요청에 다 실행됩니다.');
+  next();
+});
+
+app.get('/', (req, res, next) => {
   // res.send('Hello, Express');
   res.sendFile(path.join(__dirname, '/index.html'));
 });
+
+
 
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기 중');
--- ch6/6.2/learn-express/app.js
+++ ch6/6.2/learn-express/app.js
@@ -5,11 +5,27 @@ const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
 
+/**
+ * 현재 물리경로에 .env 파일을 읽어서 
+ * process.env 로 만든다 ( 무슨 소리??? )
+ */
 dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
+/**
+ * app.user next() 내부적으로 호출함..
+ */
+
+/**
+ * morgan : dev, combined, common, short, tiny
+ * 오류 발생시 처리시간등을 로그로 출력한다.
+ */
 app.use(morgan('dev'));
+
+/**
+ * 정적인 파일들을 제공하는 라우터 역할
+ */
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -54,16 +70,26 @@ app.post('/upload', upload.single('image'), (req, res) => {
   res.send('ok');
 });
 
-app.get('/', (req, res, next) => {
-  console.log('GET / 요청에서만 실행됩니다.');
-  next();
-}, (req, res) => {
-  throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
-});
-app.use((err, req, res, next) => {
+
+
+app.get('/', 
+  (req, res, next) => {
+      console.log('next:', next );
+      console.log('GET / 요청에서만 실행됩니다.');
+      next();
+  }, 
+  (req, res) => {
+    throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
+  }
+);
+
+/** 에러처리 app.use 
+ * 람다의 매개변수가 4개 모두 존재해야한다. */
+app.use(
+  (err, req, res, next) => {
   console.error(err);
-  res.status(500).send(err.message);
-});
+  res.status(500).send(err.message);}
+);
 
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기 중');
